3817e2ccfab79c04dac019e476f5d68215f33202
matvey-27 <neutral_notes@tutamail.com>
2025-05-16 08:27:23 +0300

    update 0.2

--- script/startDwm.sh
+++ script/startDwm.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# переключени языка
+# sudo pacman -S xorg-xkbcomp xorg-setxkbmap
+setxkbmap -layout us,ru -option grp:win_space_toggle
+feh --bg-fill ~/dwm/wallpaper/wallpaper1.jpg
+
 dwmblocks &
 
 while true; do
