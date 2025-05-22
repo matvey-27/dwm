03295f1184920b936e28548dba1e345430bcd6ab
matvey-27 <neutral_notes@tutamail.com>
2025-05-22 14:27:00 +0300

    add graps

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
+static const unsigned int gappx = 30; // Горизонтальный отступ между окнами
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -56,15 +57,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 #include <X11/XF86keysym.h>
-#define XF86XK_AudioLowerVolume	0x1008FF11   /* Volume control down        */
-#define XF86XK_AudioMute	0x1008FF12   /* Mute sound from the system */
-#define XF86XK_AudioRaiseVolume	0x1008FF13   /* Volume control up          */
-#define XF86XK_MonBrightnessUp   0x1008FF02  /* Monitor/panel brightness */
-#define XF86XK_MonBrightnessDown 0x1008FF03  /* Monitor/panel brightness */
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL }; 
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *brupcmd[] = { "light", "-A", "10", NULL };
